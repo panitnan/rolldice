e507109f38d199bc0e71b9c229ee0d75f2f08448
panitnan <panitnan>
2019-08-13 23:27:02 +0700

    random

--- app/src/main/java/buu/informatics/s59161102/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59161102/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package buu.informatics.s59161102.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +15,22 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6)+1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun reset(){
+
     }
 }
